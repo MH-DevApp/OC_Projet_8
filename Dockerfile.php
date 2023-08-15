0a16a0849e814a83b01515130ca12bc29d2edbd4
mehdi <mehdih.devapp@gmail.com>
2023-08-15 11:52:39 +0200

    fix (#2): update dockerfile file for check:requirements  - set date.timezone  - install php5.6-curl  - install php5.6-intl  - fix installation of composer

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,5 +1,6 @@
-# Utiliser l'image officielle de PHP 5.6
 FROM ubuntu:22.04
+RUN apt-get update && apt-get install -y zip git libicu-dev locales
+RUN locale-gen fr_FR.UTF-8
 RUN apt-get update \
     && apt-get install -y curl \
     && apt-get install -y software-properties-common \
@@ -8,13 +9,12 @@ RUN apt-get update \
     && apt-get update \
     && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata \
     && apt-get install -y php5.6 \
-    && apt-get install -y php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml \
-    && rm -rf /var/lib/apt/lists/* \
-RUN curl -sS https://getcomposer.org/installer | php -- --filename=composer --install-dir=/usr/local/bin
+    && apt-get install -y php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-curl php5.6-intl \
+    && rm -rf /var/lib/apt/lists/*
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 RUN curl -sS https://get.symfony.com/cli/installer | bash
-RUN apt update && apt install -y zip git libicu-dev locales
 RUN mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
-RUN locale-gen fr_FR.UTF-8
+RUN sed -ri -e 's!;date.timezone =!date.timezone = "Europe/Paris"!g' /etc/php/5.6/cli/php.ini
 WORKDIR /app
 COPY . .
 RUN symfony composer install --no-scripts
