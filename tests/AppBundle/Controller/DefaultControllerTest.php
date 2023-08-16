4d5590ddcff7e331697df1617b938edde673bc69
mehdi <mehdih.devapp@gmail.com>
2023-08-16 02:13:20 +0200

    feat (#6): [V3.1] functional tests setup

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,16 +3,53 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\AppBundle\UtilsTests\UtilsTests;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    private $client = null;
+
+    private $urlGenerator = null;
+
+    public function setUp()
     {
-        $client = static::createClient();
+        $this->client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->urlGenerator = $this->client
+            ->getContainer()
+            ->get('router');
     }
+
+    public function testHomePageWithoutBeingAuthenticated()
+    {
+        $this->client->request(
+            Request::METHOD_GET,
+            $this->urlGenerator->generate('homepage')
+        );
+
+        $this->assertEquals(Response::HTTP_FOUND, $this->client->getResponse()->getStatusCode());
+
+        $this->client->followRedirect();
+
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals('/login', $this->client->getRequest()->getPathInfo());
+    }
+
+    public function testHomePageWhileBeingAuthenticated()
+    {
+        $this->client = UtilsTests::createAuthenticatedClient($this->client, 'user');
+        $crawler = $this->client->getCrawler();
+
+        $info = $crawler->filter('h1')->text();
+        $info = trim(preg_replace('/\s\s+/', ' ', $info));
+
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+        $this->assertSame(
+            'Bienvenue sur Todo List, l\'application vous permettant de gérer l\'ensemble de vos tâches sans effort !',
+            $info
+        );
+    }
+
 }
