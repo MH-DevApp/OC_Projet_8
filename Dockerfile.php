2b9bc723361912ca846f3f2411801c21a799a9ab
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-08-16 02:23:58 +0200

    feat (#15): [V3.1] Test setup : Functional tests (#6)
    * fix (#2): [V3.1] install xdebug for coverage of tests
    
    * feat (#6): [V3.1] setting config files for tests and add scripts for command to launch tests in composer file
    
    * fix (#6): [V3.1] fix code in unit tests
    
    * feat (#6): [V3.1] functional tests setup
    
    * feat (#6): [V3.1] coverage repo in web folder for accessible in browser app
    
    * feat (#6): [V3.1] pictures coverage in audit folder

--- Dockerfile.php
+++ Dockerfile.php
@@ -9,12 +9,15 @@ RUN apt-get update \
     && apt-get update \
     && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata \
     && apt-get install -y php5.6 \
-    && apt-get install -y php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-curl php5.6-intl \
+    && apt-get install -y php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-xml php5.6-curl php5.6-intl php5.6-xdebug \
     && rm -rf /var/lib/apt/lists/*
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 RUN curl -sS https://get.symfony.com/cli/installer | bash
 RUN mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
 RUN sed -ri -e 's!;date.timezone =!date.timezone = "Europe/Paris"!g' /etc/php/5.6/cli/php.ini
+RUN echo "zend_extension=xdebug.so" >> /etc/php/5.6/cli/php.ini \
+    && echo "xdebug.remote_enable=1" >> /etc/php/5.6/cli/php.ini \
+    && echo "xdebug.remote_autostart=1" >> /etc/php/5.6/cli/php.ini
 WORKDIR /app
 COPY . .
 RUN symfony composer install --no-scripts
